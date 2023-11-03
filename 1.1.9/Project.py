7675c8734443c4c0781c08472dc7e18d1d73de25
21933 <angelo.llanos@vtsd.com>
2023-11-03 08:41:28 -0400

    Test file

--- 1.1.9/Project.py
+++ 1.1.9/Project.py
@@ -1,27 +1,34 @@
 import turtle as trtl
 painter = trtl.Turtle()
-painter.penup()
-painter.goto(-200, 0)
-painter.pendown()
-
-x = -200
-y = 0
-move_x = 1
-move_y = 1
-while (x < 0):
-
-    while (y < 100):
-        x = x + move_x
-        y = y + move_y
-        painter.goto(x, y)
-    move_y = -1
-
-    while (y > 0):
-        x = x - move_x
-        y = y - move_y
-        painter.goto(x, y)
-    move_y = 1
-
+painter.speed(50)
+painter.width(3)
+painter.up()
+painter.goto(-150, 50)
+painter.down()
+painter.left(30)
+#Draw first layer
+for line in range(6):
+    for line in range(4):
+        painter.forward(200)
+        painter.right(90)
+    for line in range(1):
+        painter.forward(50)
+        painter.left(60)
+        painter.forward(50)
+        for line in range(4):
+            painter.right(90)
+            painter.forward(200)
+    for line in range(1):
+        painter.right(90)
+        painter.forward(50)
+        painter.left(60)
+        painter.forward(50)
+        painter.right(90)
+#Draw Flower
+painter.right(30)
+painter.up()
+painter.goto(0, 0)
+painter.down()
 
 wn = trtl.Screen()
 wn.mainloop()
